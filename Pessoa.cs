6c248c9bc88aa1f86b24ef067dba295688c04700
Paulo Henrique <zs.phenriqueprofessional@gmail.com>
2022-12-05 10:52:27 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comportamento_de_Memoria
+{
+    public class Pessoa
+    {
+        private string _name;
+        private int Registration { get; set; }
+
+        private string? _project;
+
+        public Pessoa(string name, int registration, string? project)
+        {
+            _name = name;
+            Registration = registration;
+            _project = project;
+        }
+
+
+        public override string ToString()
+        {
+            if(_project != null)
+            {
+                return "\nName: " + _name + "\nRegistration: " + Registration + "\nProject: "+ _project;
+            }
+            else
+            {
+                return "\nName: " + _name +
+                       "\nRegistration: " + Registration +
+                       "\nProject: No project";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Comportamento_de_Memoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, esc;
+            int registration;
+
+            string? project;
+
+            Console.Write("Type your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Type your registration: ");
+            registration = int.Parse(Console.ReadLine());
+
+            Console.Write("Are you participating in a project (y/n)? ");
+            esc = Console.ReadLine();
+
+            if (esc == "y")
+            {
+                Console.Write("Type the name of your project: ");
+                project= Console.ReadLine();
+            }
+            else
+            {
+                project = null;
+            }
+
+            Pessoa p = new Pessoa(name, registration, project);
+            Console.WriteLine(p);
+        }
+    }
+}
